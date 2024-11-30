4a02d2a5d888f8b7415b97daa68e0aeb737a3093
SELXXLBG <152442745+SELXXLBG@users.noreply.github.com>
2024-11-30 21:42:37 +0100

    Update Calculateur de moyene.py

--- Calculateur de moyene.py
+++ Calculateur de moyene.py
@@ -1,9 +1,10 @@
-nbm_de_notes = int (10)
+nbm_de_notes = 11
 notes = []
 
-for i in range(10) :
+for i in range (nbm_de_notes) :
     note = float(input("met une de tes moyenne de matière (sur 20) n'importe laquelle : "))
+    
     notes.append(note)
 moyenne_gnrl = sum(notes) / nbm_de_notes
 
-print(f" ta moyenne générale est donc de {moyenne_gnrl: .2f14} ! ")
+print(f" ta moyenne générale est donc de {moyenne_gnrl} ! ")
